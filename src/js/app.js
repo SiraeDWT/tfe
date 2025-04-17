9d956420ca57d57deb5bac5b1b4db2dc5adf12ce
Dylan <dylanv.dwt@hotmail.com>
2025-04-17 22:58:23 +0200

    Optimize gsap

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,5 @@
+"use strict";
+
 import '../css/app.css';
 import '../scss/app.scss';
 
@@ -14,50 +16,55 @@ gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
 // navbar();
 
-
+const index = document.querySelector('.home');
 const tl = gsap.timeline();
+const mm = gsap.matchMedia();
 
-tl.fromTo("#transition p", 
-          { x: "-500%", display: "block" }, 
-          { x: "0", duration: 1, ease: "power4.out" }
-         )
-  .to("#transition p", { 
-  fontSize: "2560px", 
-  duration: 0.4, 
-  ease: "power4.in" 
-})
-  .to("#transition", { 
-    opacity: 0, 
-    duration: 0.1, 
-    onComplete: () => {
-        document.getElementById("transition").style.display = "none";
-    }
-});
-
-
-
-
-tl.to("#lh-img-01", {
-    x: 0,
-    opacity: 1,
-    duration: 0.4,
-    ease: "back.out(1.7)"
-})
-.to("#lh-img-02", {
-    x: 0,
-    opacity: 1,
-    duration: 0.4,
-    ease: "back.out(1.7)"
-}, "+=0.2");
-
-
-const lines = document.querySelectorAll('.svg-line');
-
-lines.forEach((line) => {
-    tl.to(line, {
-        x: 0,
-        opacity: 1,
-        duration: 0.4,
-        ease: "back.out(1.7)"
+if(index){
+    mm.add("(max-width: 1439px)", () => {});
+    
+    mm.add("(min-width: 1440px)", () => {
+        tl.fromTo("#transition p", 
+            { x: "-500%", display: "block" }, 
+            { x: "0", duration: 1, ease: "power4.out" }
+              )
+      .to("#transition p", { 
+          fontSize: "2560px", 
+          duration: 0.4, 
+          ease: "power4.in" 
+      })
+      .to("#transition", { 
+          opacity: 0, 
+          duration: 0.1, 
+          onComplete: () => {
+              document.getElementById("transition").style.display = "none";
+          }
+      });
+  
+  
+      tl.to("#lh-img-01", {
+          x: 0,
+          opacity: 1,
+          duration: 0.4,
+          ease: "back.out(1.7)"
+      })
+      .to("#lh-img-02", {
+          x: 0,
+          opacity: 1,
+          duration: 0.4,
+          ease: "back.out(1.7)"
+      }, "+=0.2");
+  
+  
+      const lines = document.querySelectorAll('.svg-line');
+  
+      lines.forEach((line) => {
+          tl.to(line, {
+              x: 0,
+              opacity: 1,
+              duration: 0.4,
+              ease: "back.out(1.7)"
+          });
+      });
     });
-});
+}
