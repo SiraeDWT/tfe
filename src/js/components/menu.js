b5c2644138786da597372de0015fb6330585b104
Dylan <dylanv.dwt@hotmail.com>
2025-04-17 17:50:53 +0200

    Add menu

--- src/js/components/menu.js
+++ src/js/components/menu.js
@@ -1,1 +1,19 @@
-// Script
+// export function navbar(){
+//     const menu = document.querySelector('.menu');
+//     const home = document.querySelector('#home');
+
+//     const observer = new IntersectionObserver(
+//         ([entry]) => {
+//         if (!entry.isIntersecting) {
+//             menu.classList.remove('opacity-0', 'pointer-events-none');
+//             menu.classList.add('opacity-100', 'pointer-events-auto');
+//         } else {
+//             menu.classList.add('opacity-0', 'pointer-events-none');
+//             menu.classList.remove('opacity-100', 'pointer-events-auto');
+//         }
+//         },
+//         { threshold: 0 }
+//     );
+
+//     observer.observe(home);
+// }
