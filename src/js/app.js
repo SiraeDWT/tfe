562fbc7fda835f2c43f79c9f8f5291750e31e081
Dylan <dylanv.dwt@hotmail.com>
2025-04-23 16:20:48 +0200

    Add citation

--- src/js/app.js
+++ src/js/app.js
@@ -73,5 +73,75 @@ mm.add("(min-width: 1440px)", () => {
                 ease: "back.out(1.7)"
             });
         });
+
+        
+
+
+        // const targetText = "« Le moment où vous abandonnez, c'est le moment où vous laissez quelqu'un d'autre gagner. »";
+        // const quoteEl = document.querySelector("#quote");
+
+        // gsap.set(quoteEl, { opacity: 0 });
+
+        // ScrollTrigger.create({
+        //     trigger: "#citation",
+        //     start: "top 90%",
+        //     once: true,
+        //     onEnter: () => {
+        //         gsap.to(quoteEl, {
+        //             opacity: 1,
+        //             duration: 0.3,
+        //             onComplete: () => typeText(targetText)
+        //         });
+        //     }
+        // });
+
+        // function typeText(text) {
+        //     quoteEl.textContent = "";
+        //     let index = 0;
+        //     const interval = setInterval(() => {
+        //         quoteEl.textContent += text.charAt(index);
+        //         index++;
+        //         if (index >= text.length) clearInterval(interval);
+        //     }, 30);
+        // }
+
+        const targetText = "« Le moment où vous abandonnez, c'est le moment où vous laissez quelqu'un d'autre gagner. »";
+        const quoteEl = document.getElementById("quote");
+        const lhEl = document.querySelector(".citation__lh");
+
+        gsap.set(quoteEl, { opacity: 0 });
+        gsap.set(lhEl, { opacity: 0 });
+
+        ScrollTrigger.create({
+            trigger: "#citation",
+            start: "top 40%",
+            once: true,
+            onEnter: () => {
+                gsap.to(quoteEl, {
+                    opacity: 1,
+                    duration: 0.3,
+                    onComplete: () => typeText(targetText)
+                });
+            }
+        });
+
+        function typeText(text) {
+            quoteEl.textContent = "";
+            let index = 0;
+            const interval = setInterval(() => {
+                quoteEl.textContent += text.charAt(index);
+                index++;
+                if (index >= text.length) {
+                    clearInterval(interval);
+                    gsap.to(lhEl, {
+                        opacity: 1,
+                        duration: 1
+                    });
+                }
+            }, 50);
+        }
+
+
+
     }
 });
