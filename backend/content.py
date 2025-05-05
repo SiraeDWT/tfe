7cd2d2f3b2ba7708f4731495d94c64af61b94573
Dylan <dylanv.dwt@hotmail.com>
2025-05-05 22:50:04 +0200

    Add retina for timeline sliders

--- backend/content.py
+++ backend/content.py
@@ -9,9 +9,10 @@ history = {
                     "C'est une année de records car jamais un rookie n'avait été aussi proche de remporter le titre dès sa première tentative. Il échoue à seulement un point lors de la dernière course à Interlagos, terminant à égalité avec Alonso.",
                     "Un début prometteur, et une saison qui laisse présager une grande carrière."
                 ],
-                "image": [
-                    "images/test.jpg",
-                    "images/lh-ferrari.webp",
+                "images": [
+                    "images/parcours/hamilton-et-alonso-2007.webp",
+                    "images/parcours/mclaren-hamilton-2007.webp",
+                    "images/parcours/victoire-hamilton-2007.webp",
                 ]
             },
 
@@ -21,7 +22,7 @@ history = {
                     "Lors de la saison suivante Hamilton accueille un nouveau coéquipier, Heikki Kovalainen.",
                     "Animé d'une grande détermination après la frustration de 2007, Lewis enchaîne victoires et podiums. Il remporte son premier titre mondial dans une situation exceptionnelle. Dans le dernier virage du dernier tour à Interlagos, sous la pluie, il arrache la couronne mondiale à Felipe Massa pour devenir à 23 ans, le plus jeune champion du monde de l'époque."
                 ],
-                "image": [
+                "images": [
                     "images/",
                     ""
                 ]
@@ -33,7 +34,7 @@ history = {
                     "Les changements réglementaires bouleversent l’ordre établi et McLaren peine à trouver son rythme. C’est un début d'année difficile mais Hamilton refuse de baisser les bras, signant deux magnifiques victoires en Hongrie et à Singapour.",
                     "Il termine 5e du championnat, rappelant au monde qu’il est toujours là, prêt à se battre quelles que soient les circonstances."
                 ],
-                "image": [
+                "images": [
                     "images/",
                     ""
                 ]
@@ -46,7 +47,7 @@ history = {
                     "Il frôle le titre jusqu’au dernier Grand Prix, mais finit finalement 4e avec 240 points. 2011 est encore plus frustrante, impacté par des accrochages répétés avec Felipe Massa et une saison moyenne.",
                     "En 2012, après une nouvelle saison compliquée et malgré plusieurs victoires, Hamilton surprend le monde en annonçant son départ de McLaren, l’écurie qui l'a formé depuis son adolescence, pour rejoindre la future prestigieuse écurie Mercedes."
                 ],
-                "image": [
+                "images": [
                     "images/",
                     ""
                 ]
@@ -62,7 +63,7 @@ history = {
                     "Hamilton arrive chez Mercedes AMG Petronas, succédant à la légende Michael Schumacher et rejoint son ami d’enfance, Nico Rosberg. Cette saison marque le début d’une nouvelle ère.",
                     "Il remporte une victoire symbolique en Hongrie, signe d’espoir pour l’avenir. Il termine à la 4e place du championnat derrière le 4 fois Champion du Monde, Sébastian Vettel mais se prépare déjà pour revenir plus fort que jamais."
                 ],
-                "image": [
+                "images": [
                     "images/",
                     ""
                 ]
@@ -74,7 +75,7 @@ history = {
                     "Avec l’arrivée des moteurs hybrides V6 turbo, Mercedes s'impose comme la force dominante de la Formule 1. Une rivalité intense commence entre Lewis et Nico, désormais rivaux.",
                     "Hamilton remporte brillamment son deuxième titre mondial en 2014, avec 11 victoires et 384 points. En 2015, il domine encore plus nettement et égale son idole de toujours, Ayrton Senna, en décrochant son troisième titre mondial. Lewis est au sommet de sa forme."
                 ],
-                "image": [
+                "images": [
                     "images/",
                     ""
                 ]
@@ -87,7 +88,7 @@ history = {
                     "Incidents, tensions et rivalités ponctuent toute la saison. Hamilton, trahi par plusieurs problèmes mécaniques, voit Rosberg lui arracher le titre pour seulement cinq points. Rosberg, épuisé par la lutte, annonce sa retraite surprise à peine titré.",
                     "Un moment amer pour Lewis, mais qui pousse encore plus haut sa détermination pour la suite de sa carrière."
                 ],
-                "image": [
+                "images": [
                     "images/",
                     ""
                 ]
@@ -100,7 +101,7 @@ history = {
                     "Il remporte quatre titres consécutifs et atteint un total de sept titres mondiaux, égalant Michael Schumacher, et bat même son record de victoires en Grand Prix.",
                     "Le Britannique est une légende vivante du sport automobile et fait naître de plus en plus de passion."
                 ],
-                "image": [
+                "images": [
                     "images/",
                     ""
                 ]
@@ -113,7 +114,7 @@ history = {
                     "Le titre se joue jusqu’au dernier virage du Grand Prix d’Abu Dhabi, où une décision controversée prive Hamilton d’un huitième titre historique. Lewis termine vice-champion mais gagne définitivement le cœur de nombreux fans grâce à son fair-play et son sang-froid dans l’adversité.",
                     "Cette saison est probablement la meilleure de tous les temps."
                 ],
-                "image": [
+                "images": [
                     "images/",
                     ""
                 ]
@@ -126,7 +127,7 @@ history = {
                     "En 2023, il remontant régulièrement sur le podium et termine troisième au championnat. Enfin, en 2024, Hamilton retrouve le chemin de la victoire avec deux victoires incroyables. Sa combativité et son talent sont toujours bien présent.",
                     "Cette année-là, Hamilton bouleverse encore une fois la F1 en annonçant son départ de Mercedes après 12 saisons historiques pour relever un défi encore plus prestigieux : rejoindre la mythique Scuderia Ferrari en 2025. Une nouvelle aventure qui s’annonce légendaire."
                 ],
-                "image": [
+                "images": [
                     "images/",
                     ""
                 ]
@@ -142,7 +143,7 @@ history = {
                     "Après avoir littéralement retourné le monde du sport automobile avec ce transfert totalement inattendu, Lewis Hamilton fait face à un tout nouveau défis : remporter son huitième titre et faire renouer Ferrari avec une victoire de champion du monde.",
                     "La dernière champion du monde avec une Ferrari remonte à 2007, l’année où il a commencé et où il a perdu son potentiel premier titre face à Kimi Raikkonen, alors en rouge à ce moment-là."
                 ],
-                "image": [
+                "images": [
                     "images/lh-ferrari.webp",
                     ""
                 ]
