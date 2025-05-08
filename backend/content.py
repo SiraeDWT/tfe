794d75b881208fc23827d86e2d5a7db07333fa1a
Dylan <dylanv.dwt@hotmail.com>
2025-05-08 15:51:51 +0200

    Add microtypo

--- backend/content.py
+++ backend/content.py
@@ -4,9 +4,9 @@ history = {
             "2007": {
                 "title": "Départ d’un rêve",
                 "story": [
-                    "Lewis Hamilton réalise son rêve d’enfance et débute sa carrière en Formule 1 à 22 ans chez McLaren, aux côtés du double champion du monde Fernando Alonso.",
+                    "Lewis&nbsp;Hamilton réalise son rêve d’enfance et débute sa carrière en Formule&nbsp;1 à 22 ans chez McLaren, aux côtés du double champion du monde Fernando&nbsp;Alonso.",
                     "Dès sa première saison, Lewis marque les fans avec son style agressif, sa vitesse et sa maturité.",
-                    "C'est une année de records car jamais un rookie n'avait été aussi proche de remporter le titre dès sa première tentative. Il échoue à seulement un point lors de la dernière course à Interlagos, terminant à égalité avec Alonso.",
+                    "C’est une année de records car jamais un rookie n’avait été aussi proche de remporter le titre dès sa première tentative. Il échoue à seulement un point lors de la dernière course à Interlagos, terminant à égalité avec Alonso.",
                     "Un début prometteur, et une saison qui laisse présager une grande carrière."
                 ],
                 "images": [
@@ -19,9 +19,9 @@ history = {
             "2008": {
                 "title": "Le sacre historique",
                 "story": [
-                    "Lors de la saison suivante Hamilton accueille un nouveau coéquipier, Heikki Kovalainen.",
-                    "Animé d'une grande détermination après la frustration de 2007, Lewis enchaîne victoires et podiums. Il remporte son premier titre mondial dans une situation exceptionnelle.",
-                    "Dans le dernier virage du dernier tour à Interlagos, sous la pluie, il arrache la couronne mondiale à Felipe Massa pour devenir à 23 ans, le plus jeune champion du monde de l'époque."
+                    "Lors de la saison suivante Hamilton accueille un nouveau coéquipier, Heikki&nbsp;Kovalainen.",
+                    "Animé d’une grande détermination après la frustration de 2007, Lewis enchaîne victoires et podiums. Il remporte son premier titre mondial dans une situation exceptionnelle.",
+                    "Dans le dernier virage du dernier tour à Interlagos, sous la pluie, il arrache la couronne mondiale à Felipe&nbsp;Massa pour devenir à 23 ans, le plus jeune champion du monde de l’époque."
                 ],
                 "images": [
                     "images/parcours/2008/hamilton-celebration-2008.webp",
@@ -33,7 +33,7 @@ history = {
             "2009": {
                 "title": "Saison de transition",
                 "story": [
-                    "Les changements réglementaires bouleversent l’ordre établi et McLaren peine à trouver son rythme. C’est un début d'année difficile mais Hamilton refuse de baisser les bras, signant deux magnifiques victoires en Hongrie et à Singapour.",
+                    "Les changements réglementaires bouleversent l’ordre établi et McLaren peine à trouver son rythme. C’est un début d’année difficile mais Hamilton refuse de baisser les bras, signant deux magnifiques victoires en Hongrie et à Singapour.",
                     "Il termine 5e du championnat, rappelant au monde qu’il est toujours là, prêt à se battre quelles que soient les circonstances."
                 ],
                 "images": [
@@ -46,9 +46,9 @@ history = {
             "2010-2012": {
                 "title": "Frustration",
                 "story": [
-                    "En 2010, Lewis accueille chez McLaren un nouveau partenaire, le champion du monde en titre, Jenson Button. Malgré des monoplaces compétitives, Hamilton doit affronter la montée en puissance de Sebastian Vettel et Red Bull.",
-                    "Il frôle le titre jusqu’au dernier Grand Prix, mais finit finalement 4e avec 240 points. 2011 est encore plus frustrante, impacté par des accrochages répétés avec Felipe Massa et une saison moyenne.",
-                    "En 2012, après une nouvelle saison compliquée et malgré plusieurs victoires, Hamilton surprend le monde en annonçant son départ de McLaren, l’écurie qui l'a formé depuis son adolescence, pour rejoindre la future prestigieuse écurie Mercedes."
+                    "En 2010, Lewis accueille chez McLaren un nouveau partenaire, le champion du monde en titre, Jenson&nbsp;Button. Malgré des monoplaces compétitives, Hamilton doit affronter la montée en puissance de Sebastian&nbsp;Vettel et Red&nbsp;Bull.",
+                    "Il frôle le titre jusqu’au dernier Grand&nbsp;Prix, mais finit finalement 4e avec 240 points. 2011 est encore plus frustrante, impacté par des accrochages répétés avec Felipe&nbsp;Massa et une saison moyenne.",
+                    "En 2012, après une nouvelle saison compliquée et malgré plusieurs victoires, Hamilton surprend le monde en annonçant son départ de McLaren, l’écurie qui l’a formé depuis son adolescence, pour rejoindre la future prestigieuse écurie&nbsp;Mercedes."
                 ],
                 "images": [
                     "images/parcours/2010-2012/hamilton-button-2010.webp",
@@ -64,8 +64,8 @@ history = {
             "2013": {
                 "title": "Arrivée chez Mercedes",
                 "story": [
-                    "Hamilton arrive chez Mercedes AMG Petronas, succédant à la légende Michael Schumacher et rejoint son ami d’enfance, Nico Rosberg. Cette saison marque le début d’une nouvelle ère.",
-                    "Il remporte une victoire symbolique en Hongrie, signe d’espoir pour l’avenir. Il termine à la 4e place du championnat derrière le 4 fois Champion du Monde, Sébastian Vettel mais se prépare déjà pour revenir plus fort que jamais."
+                    "Hamilton arrive chez Mercedes&nbsp;AMG&nbsp;Petronas, succédant à la légende Michael&nbsp;Schumacher et rejoint son ami d’enfance, Nico&nbsp;Rosberg. Cette saison marque le début d’une nouvelle ère.",
+                    "Il remporte une victoire symbolique en Hongrie, signe d’espoir pour l’avenir. Il termine à la 4e place du championnat derrière le 4 fois Champion du Monde, Sébastian&nbsp;Vettel mais se prépare déjà pour revenir plus fort que jamais."
                 ],
                 "images": [
                     "images/parcours/2013/hamilton-mercedes-2013.webp",
@@ -78,8 +78,8 @@ history = {
             "2014-2015": {
                 "title": "Domination et rivalité",
                 "story": [
-                    "Avec l’arrivée des moteurs hybrides V6 turbo, Mercedes s'impose comme la force dominante de la Formule 1. Une rivalité intense commence entre Lewis et Nico, désormais rivaux.",
-                    "Hamilton remporte brillamment son deuxième titre mondial en 2014, avec 11 victoires et 384 points. En 2015, il domine encore plus nettement et égale son idole de toujours, Ayrton Senna, en décrochant son troisième titre mondial. Lewis est au sommet de sa forme."
+                    "Avec l’arrivée des moteurs hybrides V6 turbo, Mercedes s’impose comme la force dominante de la Formule&nbsp;1. Une rivalité intense commence entre Lewis et Nico, désormais rivaux.",
+                    "Hamilton remporte brillamment son deuxième titre mondial en 2014, avec 11 victoires et 384 points. En 2015, il domine encore plus nettement et égale son idole de toujours, Ayrton&nbsp;Senna, en décrochant son troisième titre mondial. Lewis est au sommet de sa forme."
                 ],
                 "images": [
                     "images/parcours/2014-2015/hamilton-2014.webp",
@@ -108,10 +108,10 @@ history = {
             },
             
             "2017-2020": {
-                "title": "L'ère Hamilton",
+                "title": "L’ère Hamilton",
                 "story": [
-                    "Valtteri Bottas remplace Rosberg chez Mercedes. Hamilton étant désormais le leader incontesté de Mercedes, entre dans une ère de domination écrasante.",
-                    "Il remporte quatre titres consécutifs et atteint un total de sept titres mondiaux, égalant Michael Schumacher, et bat même son record de victoires en Grand Prix.",
+                    "Valtteri&nbsp;Bottas remplace Rosberg chez Mercedes. Hamilton étant désormais le leader incontesté de Mercedes, entre dans une ère de domination écrasante.",
+                    "Il remporte quatre titres consécutifs et atteint un total de sept titres mondiaux, égalant Michael Schumacher, et bat même son record de victoires en Grand&nbsp;Prix.",
                     "Le Britannique est une légende vivante du sport automobile et fait naître de plus en plus de passion."
                 ],
                 "images": [
@@ -127,8 +127,8 @@ history = {
             "2021": {
                 "title": "Duel historique",
                 "story": [
-                    "Hamilton affronte son adversaire le plus redoutable : Max Verstappen. La saison est intense, dramatique, pleine d'incidents et de controverses. Leur rivalité pousse chacun à leurs limites, provoquant l’histérie de millions de fans à travers le monde.",
-                    "Le titre se joue jusqu’au dernier virage du Grand Prix d’Abu Dhabi, où une décision controversée prive Hamilton d’un huitième titre historique. Lewis termine vice-champion mais gagne définitivement le cœur de nombreux fans grâce à son fair-play et son sang-froid dans l’adversité.",
+                    "Hamilton affronte son adversaire le plus redoutable&nbsp;: Max&nbsp;Verstappen. La saison est intense, dramatique, pleine d’incidents et de controverses. Leur rivalité pousse chacun à leurs limites, provoquant l’histérie de millions de fans à travers le monde.",
+                    "Le titre se joue jusqu’au dernier virage du Grand&nbsp;Prix d’Abu&nbsp;Dhabi, où une décision controversée prive Hamilton d’un huitième titre historique. Lewis termine vice-champion mais gagne définitivement le cœur de nombreux fans grâce à son fair-play et son sang-froid dans l’adversité.",
                     "Cette saison est probablement la meilleure de tous les temps."
                 ],
                 "images": [
@@ -144,9 +144,9 @@ history = {
             "2022-2024": {
                 "title": "Nouvelle ère difficile",
                 "story": [
-                    "George Russell rejoint Mercedes alors que la frustration de 2021 reste là. Avec les nouveaux règlements techniques et l'effet de sol, l’équipe n’arrive pas à proposer une monoplace compétitive. Hamilton, pour la première fois de sa carrière, ne remporte aucune victoire en 2022 à cause de la pire voiture de Mercedes depuis leur début.",
+                    "George&nbsp;Russell rejoint Mercedes alors que la frustration de 2021 reste là. Avec les nouveaux règlements techniques et l’effet de sol, l’équipe n’arrive pas à proposer une monoplace compétitive. Hamilton, pour la première fois de sa carrière, ne remporte aucune victoire en 2022 à cause de la pire voiture de Mercedes depuis leur début.",
                     "En 2023, il remontant régulièrement sur le podium et termine troisième au championnat. Enfin, en 2024, Hamilton retrouve le chemin de la victoire avec deux victoires incroyables. Sa combativité et son talent sont toujours bien présent.",
-                    "Cette année-là, Hamilton bouleverse encore une fois la F1 en annonçant son départ de Mercedes après 12 saisons historiques pour relever un défi encore plus prestigieux : rejoindre la mythique Scuderia Ferrari en 2025. Une nouvelle aventure qui s’annonce légendaire."
+                    "Cette année-là, Hamilton bouleverse encore une fois la F1 en annonçant son départ de Mercedes après 12 saisons historiques pour relever un défi encore plus prestigieux&nbsp;: rejoindre la mythique Scuderia&nbsp;Ferrari en 2025. Une nouvelle aventure qui s’annonce légendaire."
                 ],
                 "images": [
                     "images/parcours/2022-2024/hamilton-mercedes-w13-2022.webp",
@@ -165,8 +165,8 @@ history = {
             "2025": {
                 "title": "Transfert légendaire",
                 "story": [
-                    "Après avoir littéralement retourné le monde du sport automobile avec ce transfert totalement inattendu, Lewis Hamilton fait face à un tout nouveau défis : remporter son huitième titre et faire renouer Ferrari avec une victoire de champion du monde.",
-                    "La dernière champion du monde avec une Ferrari remonte à 2007, l’année où il a commencé et où il a perdu son potentiel premier titre face à Kimi Raikkonen, alors en rouge à ce moment-là."
+                    "Après avoir littéralement retourné le monde du sport automobile avec ce transfert totalement inattendu, Lewis&nbsp;Hamilton fait face à un tout nouveau défis&nbsp;: remporter son huitième titre et faire renouer Ferrari avec une victoire de champion&nbsp;du&nbsp;monde.",
+                    "Le dernière champion&nbsp;du&nbsp;monde avec une Ferrari remonte à 2007, l’année où il a commencé et où il a perdu son potentiel premier titre face à Kimi&nbsp;Raikkonen, alors en rouge à ce moment-là."
                 ],
                 "images": [
                     "images/parcours/2025/hamilton-casque-ferrari.webp",
