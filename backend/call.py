fdbe70c515e06e3f40194e6edad790aac1254784
Dylan <dylanv.dwt@hotmail.com>
2025-05-13 21:14:33 +0200

    Optimize backend loading

--- backend/call.py
+++ backend/call.py
@@ -1,24 +1,36 @@
 import requests
 from collections import defaultdict
 
-ERGAST_URL = "http://ergast.com/api/f1/driverStandings/1.json?limit=1000"
+# ERGAST_URL = "http://ergast.com/api/f1/driverStandings/1.json?limit=1000"
+
+# def get_top_champions(limit=5):
+#     response = requests.get(ERGAST_URL)
+#     data = response.json()
+#     lists = data["MRData"]["StandingsTable"]["StandingsLists"]
+
+#     champions = defaultdict(list)
+
+#     for item in lists:
+#         driver = item["DriverStandings"][0]["Driver"]
+#         name = f"{driver['givenName']} {driver['familyName']}"
+#         champions[name].append(item["season"])
+
+#     sorted_champions = sorted(champions.items(), key=lambda x: len(x[1]), reverse=True)
+#     top = sorted_champions[:limit]
+
+#     return [{"name": name, "titles": len(seasons)} for name, seasons in top]
 
 def get_top_champions(limit=5):
-    response = requests.get(ERGAST_URL)
-    data = response.json()
-    lists = data["MRData"]["StandingsTable"]["StandingsLists"]
+    champions = {
+        "Lewis Hamilton": 7,
+        "Michael Schumacher": 7,
+        "Juan Fangio": 5,
+        "Alain Prost": 4,
+        "Sebastian Vettel": 4,
+    }
 
-    champions = defaultdict(list)
-
-    for item in lists:
-        driver = item["DriverStandings"][0]["Driver"]
-        name = f"{driver['givenName']} {driver['familyName']}"
-        champions[name].append(item["season"])
-
-    sorted_champions = sorted(champions.items(), key=lambda x: len(x[1]), reverse=True)
-    top = sorted_champions[:limit]
-
-    return [{"name": name, "titles": len(seasons)} for name, seasons in top]
+    top_champions = sorted(champions.items(), key=lambda x: x[1], reverse=True)[:limit]
+    return [{"name": name, "titles": champion} for name, champion in top_champions]
 
 
 def get_top_winners(limit=5):
